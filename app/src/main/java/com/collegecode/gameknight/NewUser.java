3c646a9d124304b382ff2fa1e8f8a1d539dbe318
Saurabh Joshi <battlex2010@yahoo.com>
2014-11-07 19:36:04 -0500

    Completed Google Login

--- app/src/main/java/com/collegecode/gameknight/NewUser.java
+++ app/src/main/java/com/collegecode/gameknight/NewUser.java
@@ -1,11 +1,38 @@
 package com.collegecode.gameknight;
 
+import android.app.AlertDialog;
+import android.app.ProgressDialog;
+import android.content.Context;
+import android.content.DialogInterface;
+import android.content.Intent;
+import android.content.IntentSender;
+import android.os.AsyncTask;
 import android.os.Bundle;
 import android.view.View;
+import android.widget.EditText;
+
+import com.google.android.gms.common.ConnectionResult;
+import com.google.android.gms.common.api.GoogleApiClient;
+import com.google.android.gms.plus.Plus;
+import com.google.android.gms.plus.model.people.Person;
+import com.parse.ParseException;
+import com.parse.ParseUser;
+
 /**
  * Created by saurabh on 14-11-06.
  */
-public class NewUser extends BaseActivity{
+public class NewUser extends BaseActivity
+        implements GoogleApiClient.ConnectionCallbacks, GoogleApiClient.OnConnectionFailedListener{
+
+    private static final int RC_SIGN_IN = 0;
+
+    Context context;
+
+    /* Client used to interact with Google APIs. */
+    private GoogleApiClient mGoogleApiClient;
+    private boolean mIntentInProgress;
+    private ConnectionResult mConnectionResult;
+    private boolean mSignInClicked;
 
     @Override
     protected int getLayoutResource() {
@@ -15,13 +42,147 @@ public class NewUser extends BaseActivity{
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
+        context = this;
         findViewById(R.id.sign_in_button).setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
-
+                if(!mGoogleApiClient.isConnecting())
+                {
+                    mSignInClicked = true;
+                    resolveSignInError();
+                }
             }
         });
 
+        mGoogleApiClient = new GoogleApiClient.Builder(this)
+                .addConnectionCallbacks(this)
+                .addOnConnectionFailedListener(this)
+                .addApi(Plus.API)
+                .addScope(Plus.SCOPE_PLUS_LOGIN)
+                .build();
+
         showBack(false);
+    }
+
+    protected void onStart() {
+        super.onStart();
+        mGoogleApiClient.connect();
+    }
+
+    protected void onStop() {
+        super.onStop();
+        if (mGoogleApiClient.isConnected()) {
+            mGoogleApiClient.disconnect();
+        }
+    }
+
+
+    /* A helper method to resolve the current ConnectionResult error. */
+    private void resolveSignInError() {
+        if (mConnectionResult.hasResolution()) {
+            try {
+                mIntentInProgress = true;
+                startIntentSenderForResult(mConnectionResult.getResolution().getIntentSender(),
+                        RC_SIGN_IN, null, 0, 0, 0);
+            } catch (IntentSender.SendIntentException e) {
+                mIntentInProgress = false;
+                mGoogleApiClient.connect();
+            }
+        }
+    }
+
+    public void onConnectionFailed(ConnectionResult result) {
+        if (!mIntentInProgress) {
+            mConnectionResult = result;
+
+            if (mSignInClicked) {
+                resolveSignInError();
+            }
+        }
+    }
+
+    @Override
+    public void onConnected(Bundle bundle) {
+        mSignInClicked = false;
+
+        showDialog();
+    }
+
+    String username;
+    private void showDialog(){
+        AlertDialog.Builder alert = new AlertDialog.Builder(this);
+        alert.setMessage("Enter a username");
+        final EditText input = new EditText(this);
+        alert.setView(input);
+
+        alert.setPositiveButton("Ok", new DialogInterface.OnClickListener() {
+            public void onClick(DialogInterface dialog, int whichButton) {
+                username = input.getText().toString();
+                new parseLogin().execute();
+            }
+        });
+
+        alert.setNegativeButton("Cancel", new DialogInterface.OnClickListener() {
+            public void onClick(DialogInterface dialog, int whichButton) {
+            }
+        });
+
+        alert.show();
+    }
+
+    @Override
+    public void onConnectionSuspended(int i) {
+
+    }
+
+    protected void onActivityResult(int requestCode, int responseCode, Intent intent) {
+        if (requestCode == RC_SIGN_IN) {
+            if (responseCode != RESULT_OK) {
+                mSignInClicked = false;
+            }
+
+            mIntentInProgress = false;
+
+            if (!mGoogleApiClient.isConnecting()) {
+                mGoogleApiClient.connect();
+            }
+        }
+    }
+
+    private class parseLogin extends AsyncTask<Void, Void, Void>{
+        ProgressDialog dialog;
+
+        @Override
+        protected void onPreExecute() {
+            super.onPreExecute();
+            dialog = new ProgressDialog(context);
+            dialog.setCancelable(false);
+            dialog.show();
+        }
+
+        @Override
+        protected Void doInBackground(Void... voids) {
+            ParseUser user = new ParseUser();
+            if (Plus.PeopleApi.getCurrentPerson(mGoogleApiClient) != null) {
+                Person currentPerson = Plus.PeopleApi.getCurrentPerson(mGoogleApiClient);
+                String personPhoto = currentPerson.getImage().getUrl();
+                String email = Plus.AccountApi.getAccountName(mGoogleApiClient);
+
+                user.setUsername(username);
+                user.setPassword("PASS");
+                user.setEmail(email);
+
+                try {
+                    user.signUp();
+                }catch (ParseException e){
+                    if(e.getCode() == ParseException.EMAIL_TAKEN)
+                    {
+                        /* TODO: LOGIN IN PARSE */
+                    }
+                }
+            }
+
+            return null;
+        }
     }
 }
